b58c02e17f44c0bdccfe333eaa028943b076f5a0
Damien Murtagh <damomurtagh@gmail.com>
2025-06-19 14:32:17 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import sys
+from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication
 from MainWindow import MainWindow
 
 
 def main():
+    QApplication.setAttribute(Qt.AA_EnableHighDpiScaling, True)
+    QApplication.setAttribute(Qt.AA_UseHighDpiPixmaps, True)
     app = QApplication(sys.argv)
     app.setOrganizationName("Tuxtas")
     main_win = MainWindow()
